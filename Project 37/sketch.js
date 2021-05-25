028d2c77456cae3fb6a11b769830d55d89358c07
KushalKonathala <74365407+KushalKonathala@users.noreply.github.com>
2021-05-25 14:52:42 +0530

    Add files via upload

---
+++ Project 37/sketch.js
@@ -0,0 +1,15 @@
+var PLAY = 1;
+var END = 0;
+gameState = PLAY;
+
+var trex, trexRunning, trexCollided;
+var Ground, InvisibleGround, GroundImage;
+var CloudsGroup, CloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5;
+
+var score = 0;
+var gameOver, Restart;
+
+function preload(){
+trexRunning = 
+}
